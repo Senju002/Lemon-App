28129f1c9faa3090504fdca7065238655116424c
Senju002 <rajkaran.mei34@gmail.com>
2023-12-28 18:20:49 +0700

    feedback form

--- App.js
+++ App.js
@@ -4,14 +4,15 @@ import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
 import WelcomeScreen from "./components/WelcomeScreen";
 import MenuItems from "./components/MenuItems";
+import FeedbackScreen from "./components/FeedbackScreen";
 
 export default function App() {
   return (
     <>
       <View style={{ flex: 1, backgroundColor: "#495E57" }}>
-        <LittleLemonHeader />
-        {/* <WelcomeScreen /> */}
-        <MenuItems />
+        {/* <LittleLemonHeader />
+        <MenuItems /> */}
+        <FeedbackScreen />
       </View>
       <View style={{ backgroundColor: "#495E57" }}>
         <LittleLemonFooter />
